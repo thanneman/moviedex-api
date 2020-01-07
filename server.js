08d6500f170a8dce166c4653be6653830ff346cc
thanneman <thanneman1@gmail.com>
2020-01-07 14:55:39 -0700

    Basic server built

--- server.js
+++ server.js
@@ -1,16 +1,51 @@
+require('dotenv').config();
 const express = require('express')
+const helmet = require('helmet')
 const morgan = require('morgan')
+const cors = require('cors')
+const POKEDEX = require('./moviedex.json')
 
 const app = express()
 
 app.use(morgan('dev'))
+app.use(helmet())
+app.use(cors())
 
-app.use((req, res) => {
-    res.send('Hello, world!')
-})
+app.use(function validateBearerToken(req, res, next) {
+    const apiToken = process.env.API_TOKEN;
+    const authToken = req.get('Authorization');
 
-const PORT = 8000
+    if (!authToken || authToken.split(' ')[1] !== apiToken) {
+        return res.status(401).json({ error: 'Unauthorized request' });
+    };
+
+    next()
+});
+
+function handleGetTypes(req, res) {
+    res.json(validTypes);
+};
+
+app.get('/types', handleGetTypes);
+
+function handleGetPokemon(req, res) {
+    res.send('Hello Pokemon!');
+};
+
+app.get('/pokemon', function handleGetPokemon(req, res) {
+    let response = POKEDEX.pokemon;
+
+    // filter our pokemon by name if name query param is present
+    if (req.query.name) {
+        response = response.filter(pokemon => 
+            pokemon.type.includes(req.query.type)
+        );
+    };
+    res.json(response);
+});
+
+const PORT = 8000;
 
 app.listen(PORT, () => {
     console.log(`Server listening at http://localhost:${PORT}`)
-})
+});
